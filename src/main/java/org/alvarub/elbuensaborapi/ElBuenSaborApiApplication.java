2b4acf70bf52fb661916db1e714e89bd7f87af0b
Alvaro Rubina <alvarorubina132@gmail.com>
2025-03-13 18:35:21 -0300

    commit inicial

---
+++ src/main/java/org/alvarub/elbuensaborapi/ElBuenSaborApiApplication.java
@@ -0,0 +1,13 @@
+package org.alvarub.elbuensaborapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElBuenSaborApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElBuenSaborApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alvarub/elbuensaborapi/ElBuenSaborApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alvarub.elbuensaborapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElBuenSaborApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
