5f2a3316e99b747e69b0a0e557971097159d55d9
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-04-12 20:06:18 -0300

    import formadepago

--- src/main/java/org/alvarub/elbuensaborapi/model/entity/Pedido.java
+++ src/main/java/org/alvarub/elbuensaborapi/model/entity/Pedido.java
@@ -3,8 +3,8 @@ package org.alvarub.elbuensaborapi.model.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.alvarub.elbuensaborapi.model.enums.Estado;
+import org.alvarub.elbuensaborapi.model.enums.FormaPago;
 import org.alvarub.elbuensaborapi.model.enums.TipoEnvio;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
