2a11956f70e4aa244e3fb2b19bc871b841f81b09
Alvaro Rubina <alvarorubina132@gmail.com>
2025-04-13 01:38:00 -0300

    Agregados valores a los enum Estado y TipoEnvio

--- src/main/java/org/spdgrupo/elbuensaborapi/model/enums/Estado.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/model/enums/Estado.java
@@ -1,4 +1,11 @@
 package org.spdgrupo.elbuensaborapi.model.enums;
 
 public enum Estado {
+    SOLICITADO,
+    PENDIENTE,
+    EN_PREPARACION,
+    TERMINADO,
+    EN_CAMINO,
+    ENTREGADO,
+    CANCELADO
 }
--- src/main/java/org/spdgrupo/elbuensaborapi/model/enums/TipoEnvio.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/model/enums/TipoEnvio.java
@@ -1,4 +1,6 @@
 package org.spdgrupo.elbuensaborapi.model.enums;
 
 public enum TipoEnvio {
+    RETIRO_LOCAL,
+    DELIVERY
 }
