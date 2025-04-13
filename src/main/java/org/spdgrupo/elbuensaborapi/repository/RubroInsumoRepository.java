a8c89a9d6a2731192f4a0fa7bf5b3b0eed1df7d7
Alvaro Rubina <alvarorubina132@gmail.com>
2025-04-13 18:58:55 -0300

    Creados repositorios faltantes

--- src/main/java/org/spdgrupo/elbuensaborapi/repository/RubroInsumoRepository.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/repository/RubroInsumoRepository.java
@@ -1,9 +1,9 @@
 package org.spdgrupo.elbuensaborapi.repository;
 
-import org.spdgrupo.elbuensaborapi.model.entity.Pedido;
+import org.spdgrupo.elbuensaborapi.model.entity.RubroInsumo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+public interface RubroInsumoRepository extends JpaRepository<RubroInsumo, Long> {
 }
