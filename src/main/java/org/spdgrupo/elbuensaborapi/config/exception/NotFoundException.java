bb72e3b3de08e798e2477e169c2df7f2760911ea
Alvaro Rubina <alvarorubina132@gmail.com>
2025-04-13 19:06:27 -0300

    Creado Exception Handler

---
+++ src/main/java/org/spdgrupo/elbuensaborapi/config/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.spdgrupo.elbuensaborapi.config.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
