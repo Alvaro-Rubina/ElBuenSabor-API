9e192dce874080450c5a4560da39872861fee434
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-04-14 10:32:42 -0300

    .

--- src/main/java/org/spdgrupo/elbuensaborapi/repository/DomicilioRepository.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/repository/DomicilioRepository.java
@@ -4,7 +4,8 @@ import org.spdgrupo.elbuensaborapi.model.entity.Domicilio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface DomicilioRepository extends JpaRepository<Domicilio, Long> {
-    Domicilio getElementById(Long id) throws Exception;
 }
