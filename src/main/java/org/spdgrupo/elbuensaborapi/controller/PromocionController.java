441570305a04523388c52c66b5263df46e8c8f2e
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-04-15 19:44:51 -0300

    promocion controller

---
+++ src/main/java/org/spdgrupo/elbuensaborapi/controller/PromocionController.java
@@ -0,0 +1,42 @@
+package org.spdgrupo.elbuensaborapi.controller;
+
+import org.spdgrupo.elbuensaborapi.model.dto.PromocionDTO;
+import org.spdgrupo.elbuensaborapi.service.PromocionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/promociones")
+public class PromocionController {
+
+    @Autowired
+    private PromocionService promocionService;
+
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<PromocionDTO>> getPromociones() {
+        return ResponseEntity.ok(promocionService.getAllPromociones());
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<PromocionDTO> getPromocionById(@PathVariable Long id) {
+        return ResponseEntity.ok(promocionService.getPromocionById(id));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<String> savePromocion(@RequestBody PromocionDTO promocionDTO) {
+        promocionService.savePromocion(promocionDTO);
+        return ResponseEntity.ok("Promocion guardada correctamente");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updatePromocion(@PathVariable Long id, @RequestBody PromocionDTO promocionDTO) {
+        promocionService.updatePromocion(id, promocionDTO);
+        return ResponseEntity.ok("Promocion actualizada correctamente");
+    }
+
+}
