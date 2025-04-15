397f3ca99c3d29da298d3fe4d1bbfec3e32b8ad5
Alvaro Rubina <alvarorubina132@gmail.com>
2025-04-15 19:36:41 -0300

    ,

--- src/main/java/org/spdgrupo/elbuensaborapi/controller/ClienteController.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/controller/ClienteController.java
@@ -4,10 +4,7 @@ import org.spdgrupo.elbuensaborapi.model.dto.ClienteDTO;
 import org.spdgrupo.elbuensaborapi.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,28 +16,30 @@ public class ClienteController {
     private ClienteService clienteService;
 
     @GetMapping
+    @ResponseBody
     public ResponseEntity<List<ClienteDTO>> getClientes() {
         return ResponseEntity.ok(clienteService.getAllClientes());
     }
 
     @GetMapping("/{id}")
+    @ResponseBody
     public ResponseEntity<ClienteDTO> getClienteById(Long id) {
         return ResponseEntity.ok(clienteService.getClienteById(id));
     }
 
     @PostMapping("/save")
-    public ResponseEntity<String> saveCliente(ClienteDTO clienteDTO) {
+    public ResponseEntity<String> saveCliente(@RequestBody ClienteDTO clienteDTO) {
         clienteService.saveCliente(clienteDTO);
         return ResponseEntity.ok("Cliente guardado exitosamente");
     }
 
-    @PostMapping("/update")
-    public ResponseEntity<String> updateCliente(ClienteDTO clienteDTO, Long id) {
+    @PostMapping("/update/{id}")
+    public ResponseEntity<String> updateCliente(ClienteDTO clienteDTO, @PathVariable Long id) {
         clienteService.updateCliente(clienteDTO, id);
         return ResponseEntity.ok("Cliente actualizado exitosamente");
     }
 
-    @PostMapping("/delete")
+    @PostMapping("/delete/{id}")
     public ResponseEntity<String> deleteCliente(Long id) {
         clienteService.deleteCliente(id);
         return ResponseEntity.ok("Cliente eliminado exitosamente");
