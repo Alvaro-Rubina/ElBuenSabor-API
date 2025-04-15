5505b2fe3402b74903a53b2b3a3cc93eb6b0f254
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-04-15 19:50:09 -0300

    factura controller

---
+++ src/main/java/org/spdgrupo/elbuensaborapi/controller/FacturaController.java
@@ -0,0 +1,44 @@
+package org.spdgrupo.elbuensaborapi.controller;
+
+import org.spdgrupo.elbuensaborapi.model.dto.FacturaDTO;
+import org.spdgrupo.elbuensaborapi.service.FacturaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/facturas")
+public class FacturaController {
+
+    @Autowired
+    private FacturaService facturaService;
+
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<FacturaDTO>> getFacturas() {
+        return ResponseEntity.ok(facturaService.getAllFacturas());
+    }
+
+    @GetMapping("/factura/{id}")
+    @ResponseBody
+    public ResponseEntity<FacturaDTO> getFacturaById(@PathVariable Long id) {
+        return ResponseEntity.ok(facturaService.getFacturaById(id));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<String> saveFactura(@RequestBody FacturaDTO facturaDTO) {
+        facturaService.saveFactura(facturaDTO);
+        return ResponseEntity.ok("Factura guardada correctamente");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateFactura(@PathVariable Long id, @RequestBody FacturaDTO facturaDTO) {
+        facturaService.updateFactura(id, facturaDTO);
+        return ResponseEntity.ok("Factura actualizada correctamente");
+    }
+
+
+
+}
