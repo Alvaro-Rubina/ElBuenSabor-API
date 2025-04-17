1f0b5ef1536b3e1c039d23709f67018a2b44107b
Alvaro Rubina <alvarorubina132@gmail.com>
2025-04-16 21:04:41 -0300

    Agregado metodo a PedidoRepository

--- src/main/java/org/spdgrupo/elbuensaborapi/repository/PedidoRepository.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/repository/PedidoRepository.java
@@ -4,6 +4,10 @@ import org.spdgrupo.elbuensaborapi.model.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    Optional<Pedido> findByNumero(Integer numero);
 }
