8bdbac728b25080ed54d0f20ae3a51c4fccf02c6
Alvaro Rubina <alvarorubina132@gmail.com>
2025-04-24 19:33:29 -0300

    Creado PedidoController

--- src/main/java/org/spdgrupo/elbuensaborapi/controller/PedidoController.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/controller/PedidoController.java
@@ -1,16 +1,53 @@
 package org.spdgrupo.elbuensaborapi.controller;
 
 
+import org.spdgrupo.elbuensaborapi.model.dto.PedidoDTO;
 import org.spdgrupo.elbuensaborapi.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/pedido")
+@RequestMapping("/api/pedidos")
 public class PedidoController {
 
     @Autowired
     private PedidoService pedidoService;
+
+    @PostMapping("/save")
+    public ResponseEntity<String> savePedido(@RequestBody PedidoDTO pedidoDTO) {
+        pedidoService.savePedido(pedidoDTO);
+        return ResponseEntity.ok("Pedido guardado correctamente");
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<PedidoDTO> getPedidoById(@PathVariable Long id) {
+        PedidoDTO pedidoDTO = pedidoService.getPedidoById(id);
+        return ResponseEntity.ok(pedidoDTO);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<PedidoDTO>> getAllPedidos() {
+        return ResponseEntity.ok(pedidoService.getAllPedidos());
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updatePedido(@PathVariable Long id,
+                                               @RequestBody PedidoDTO pedidoDTO) {
+        pedidoService.updatePedido(id, pedidoDTO);
+        return ResponseEntity.ok("Pedido actualizado correctamente");
+    }
+
+    @PutMapping("/agregarMin/{pedidoId}")
+    // El url quedaria algo asi: http//localhost:8080/agregarMin/1?minutos=10
+    public ResponseEntity<String> agregarTiempoAlPedido(@PathVariable Long pedidoId,
+                                                        @RequestParam Long minutos) {
+        pedidoService.agregarTiempoAlPedido(pedidoId, minutos);
+        return ResponseEntity.ok("Tiempo agregado al pedido");
+    }
 
 }
