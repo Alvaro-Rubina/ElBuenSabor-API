1c9e4f37386443a81c040a3cd862289eb20c50f5
Alvaro Rubina <alvarorubina132@gmail.com>
2025-05-02 20:38:12 -0300

    Se dividió el DetallePedidoDTO en uno para peticiones y otro para res…

---
+++ src/main/java/org/spdgrupo/elbuensaborapi/model/dto/detallepedido/DetallePedidoDTO.java
@@ -0,0 +1,35 @@
+package org.spdgrupo.elbuensaborapi.model.dto.detallepedido;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.spdgrupo.elbuensaborapi.model.dto.pedido.PedidoDTO;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class DetallePedidoDTO {
+
+    @NotNull(message = "El campo cantidad no puede ser nulo")
+    @Min(value = 1, message = "La cantidad debe ser mayor a 1")
+    private Integer cantidad;
+
+    @NotNull
+    @DecimalMin(value = "0,1", message = "El subtotal debe ser mayor a 0.1")
+    private Double subTotal;
+
+    @NotNull(message = "El campo pedidoId no puede ser nulo")
+    @Min(value = 1, message = "El campo pedidoId no puede ser menor a 1")
+    private Long pedidoId;
+
+    @NotNull(message = "El campo productoId no puede ser nulo")
+    @Min(value = 1, message = "El campo productoId no puede ser menor a 1")
+    private Long productoId;
+
+    @NotNull(message = "El campo insumoId no puede ser nulo")
+    @Min(value = 1, message = "El campo insumoId no puede ser menor a 1")
+    private Long insumoId;
+}
---
+++ src/main/java/org/spdgrupo/elbuensaborapi/model/dto/detallepedido/DetallePedidoResponseDTO.java
@@ -0,0 +1,19 @@
+package org.spdgrupo.elbuensaborapi.model.dto.detallepedido;
+
+import lombok.*;
+import org.spdgrupo.elbuensaborapi.model.dto.insumo.InsumoResponseDTO;
+import org.spdgrupo.elbuensaborapi.model.dto.producto.ProductoResponseDTO;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class DetallePedidoResponseDTO {
+
+    private Long id;
+    private Integer cantidad;
+    private Double subTotal;
+    private ProductoResponseDTO producto;
+    private InsumoResponseDTO insumo;
+}
