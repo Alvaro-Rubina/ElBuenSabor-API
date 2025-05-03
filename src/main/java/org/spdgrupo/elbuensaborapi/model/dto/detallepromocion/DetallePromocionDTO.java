1e9e69dc0acbedd37ee0971016f686de21a6f96d
Alvaro Rubina <alvarorubina132@gmail.com>
2025-05-02 23:26:05 -0300

    Se dividió el DetallePromocionDTO en uno para peticiones y otro para respuestas

---
+++ src/main/java/org/spdgrupo/elbuensaborapi/model/dto/detallepromocion/DetallePromocionDTO.java
@@ -0,0 +1,29 @@
+package org.spdgrupo.elbuensaborapi.model.dto.detallepromocion;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class DetallePromocionDTO {
+
+    @NotNull(message = "El campo cantidad no puede ser nulo")
+    @Min(value = 1, message = "El campo cantidad no puede ser menor a 1")
+    private Integer cantidad;
+
+    @NotNull(message = "El campo promocionId no puede ser nulo")
+    @Min(value = 1, message = "El campo promocionId no puede ser menor a 1")
+    private Long promocionId;
+
+    @NotNull(message = "El campo productoId no puede ser nulo")
+    @Min(value = 1, message = "El campo productoId no puede ser menor a 1")
+    private Long productoId;
+
+    @NotNull(message = "El campo insumoId no puede ser nulo")
+    @Min(value = 1, message = "El campo insumoId no puede ser menor a 1")
+    private Long insumoId;
+}
---
+++ src/main/java/org/spdgrupo/elbuensaborapi/model/dto/detallepromocion/DetallePromocionResponseDTO.java
@@ -0,0 +1,18 @@
+package org.spdgrupo.elbuensaborapi.model.dto.detallepromocion;
+
+import lombok.*;
+import org.spdgrupo.elbuensaborapi.model.dto.insumo.InsumoResponseDTO;
+import org.spdgrupo.elbuensaborapi.model.dto.producto.ProductoResponseDTO;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class DetallePromocionResponseDTO {
+
+    private Long id;
+    private Integer cantidad;
+    private ProductoResponseDTO producto;
+    private InsumoResponseDTO insumo;
+}
