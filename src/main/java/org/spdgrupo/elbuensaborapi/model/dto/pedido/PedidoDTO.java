8dca5f29d0cd4509723f6c37a42073a8f3766b4f
Alvaro Rubina <alvarorubina132@gmail.com>
2025-05-02 21:12:50 -0300

    Se dividió el PedidoDTO en uno para peticiones y otro para res…

--- src/main/java/org/spdgrupo/elbuensaborapi/model/dto/pedido/PedidoDTO.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/model/dto/pedido/PedidoDTO.java
@@ -1,9 +1,12 @@
-package org.spdgrupo.elbuensaborapi.model.dto;
+package org.spdgrupo.elbuensaborapi.model.dto.pedido;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.spdgrupo.elbuensaborapi.model.dto.domicilio.DomicilioDTO;
 import org.spdgrupo.elbuensaborapi.model.enums.Estado;
 import org.spdgrupo.elbuensaborapi.model.enums.FormaPago;
 import org.spdgrupo.elbuensaborapi.model.enums.TipoEnvio;
@@ -18,21 +21,28 @@ import java.time.LocalTime;
 @Builder
 public class PedidoDTO {
 
-    private Long id;
-    private LocalDate fecha;
-    @NotNull(message = "El campo numero no puede ser nulo")
-    @Min(value = 1, message = "El campo numero debe ser mayor a 0")
-    private Integer numero;
-    private Estado estado;
-    private LocalTime horaEstimadaFin;
+    // este campo es opcional, si no se pasa, el valor x default es SOLICITADO
+    // dejo este campo ya que este dto se usara para editar pedidos, x ende es necesario
+    private Estado estado = Estado.SOLICITADO;
     private TipoEnvio tipoEnvio;
+
     @NotNull(message = "El campo totalVenta no puede ser nulo")
     @DecimalMin(value = "0.1", message = "El campo totalVenta debe ser mayor a 0")
     private Double totalVenta;
+
     @NotNull(message = "El campo totalCosto no puede ser nulo")
     @DecimalMin(value = "0.1", message = "El campo totalCosto debe ser mayor a 0")
     private Double totalCosto;
+
+    @NotNull(message = "El campo formaPago no puede ser nulo")
     private FormaPago formaPago;
-    private ClienteDTO cliente;
-    private DomicilioDTO domicilio;
+
+    @NotNull(message = "El campo clienteId no puede ser nulo")
+    @Min(value = 1, message = "El campo clienteId no puede ser menor a 1")
+    private Long clienteId;
+
+    @NotNull(message = "El campo domicilioId no puede ser nulo")
+    @Min(value = 1, message = "El campo domicilioId no puede ser menor a 1")
+    private Long domicilioId;
+
 }
---
+++ src/main/java/org/spdgrupo/elbuensaborapi/model/dto/pedido/PedidoResponseDTO.java
@@ -0,0 +1,36 @@
+package org.spdgrupo.elbuensaborapi.model.dto.pedido;
+
+import lombok.*;
+import org.spdgrupo.elbuensaborapi.model.dto.cliente.ClienteResponseDTO;
+import org.spdgrupo.elbuensaborapi.model.dto.detallepedido.DetallePedidoResponseDTO;
+import org.spdgrupo.elbuensaborapi.model.dto.domicilio.DomicilioResponseDTO;
+import org.spdgrupo.elbuensaborapi.model.dto.factura.FacturaResponseDTO;
+import org.spdgrupo.elbuensaborapi.model.entity.Factura;
+import org.spdgrupo.elbuensaborapi.model.enums.FormaPago;
+import org.spdgrupo.elbuensaborapi.model.enums.TipoEnvio;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class PedidoResponseDTO {
+
+    private Long id;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private Integer numero;
+    private LocalTime horaEstimadaFin;
+    private TipoEnvio tipoEnvio;
+    private Double totalVenta;
+    private Double totalCosto;
+    private FormaPago formaPago;
+    private ClienteResponseDTO cliente;
+    private DomicilioResponseDTO domicilio;
+    private List<DetallePedidoResponseDTO> detallePedidos;
+    private Factura factura;
+}
