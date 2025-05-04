1846f31f6dc5061dd969fad59012341435aad4fb
Alvaro Rubina <alvarorubina132@gmail.com>
2025-05-04 01:34:30 -0300

    Eliminados restos de DetalleFactura

--- src/main/java/org/spdgrupo/elbuensaborapi/repository/DetalleFacturaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.spdgrupo.elbuensaborapi.repository;
-
-import org.spdgrupo.elbuensaborapi.model.entity.DetalleFactura;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DetalleFacturaRepository extends JpaRepository<DetalleFactura, Long> {
-}
