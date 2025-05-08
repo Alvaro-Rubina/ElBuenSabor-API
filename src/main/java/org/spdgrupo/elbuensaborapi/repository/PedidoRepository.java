5a889dbd09179489b16130dcc6665f0e13ce5402
Alvaro Rubina <alvarorubina132@gmail.com>
2025-05-08 18:11:23 -0300

    Cambio en metodo de PedidoRepository

--- src/main/java/org/spdgrupo/elbuensaborapi/repository/PedidoRepository.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/repository/PedidoRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-    Optional<Pedido> findByNumero(Integer numero);
+    Optional<Pedido> findByCodigoOrden(String codigoOrden);
 }
