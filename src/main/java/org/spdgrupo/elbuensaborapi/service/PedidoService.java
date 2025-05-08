d2a5327fbacffd457f9908d36147a71886785522
Alvaro Rubina <alvarorubina132@gmail.com>
2025-05-08 18:09:49 -0300

    Añadida logica a PedidoService

--- src/main/java/org/spdgrupo/elbuensaborapi/service/PedidoService.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/service/PedidoService.java
@@ -2,18 +2,22 @@ package org.spdgrupo.elbuensaborapi.service;
 
 import lombok.RequiredArgsConstructor;
 import org.spdgrupo.elbuensaborapi.config.exception.NotFoundException;
-import org.spdgrupo.elbuensaborapi.model.dto.PedidoDTO;
+import org.spdgrupo.elbuensaborapi.model.dto.pedido.PedidoDTO;
+import org.spdgrupo.elbuensaborapi.model.dto.pedido.PedidoResponseDTO;
+import org.spdgrupo.elbuensaborapi.model.entity.DetallePedido;
 import org.spdgrupo.elbuensaborapi.model.entity.Pedido;
+import org.spdgrupo.elbuensaborapi.model.entity.Producto;
+import org.spdgrupo.elbuensaborapi.model.enums.Estado;
 import org.spdgrupo.elbuensaborapi.repository.ClienteRepository;
 import org.spdgrupo.elbuensaborapi.repository.DomicilioRepository;
 import org.spdgrupo.elbuensaborapi.repository.PedidoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,28 +33,35 @@ public class PedidoService {
 
     public void savePedido(PedidoDTO pedidoDTO) {
         Pedido pedido = toEntity(pedidoDTO);
-        pedidoRepository.save(pedido);
-        pedido.setHoraEstimadaFin(detallePedidoService.getMayorTiempoEstimado(pedido.getId()));
-        pedidoRepository.save(pedido);
+        Pedido pedidoGuardado = pedidoRepository.save(pedido);
+
+        // Una vez guardado el pedido, se guardan los detallePedidos y se calcula el precioCosto
+        detallePedidoService.saveDetallesPedidos(pedidoDTO.getDetallePedidos(), pedidoGuardado);
+
+        pedido.setHoraEstimadaFin(calculateTiempoEstimadoPreparacion(pedidoGuardado));
+        pedidoGuardado.setTotalVenta(calculateTotalVenta(pedidoGuardado));
+        pedidoGuardado.setTotalCosto(calculateTotalCosto(pedidoGuardado));
+        pedidoRepository.save(pedidoGuardado);
     }
 
-    public PedidoDTO getPedidoById(Long id) {
+    public PedidoResponseDTO getPedidoById(Long id) {
         Pedido pedido = pedidoRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Pedido con el id " + id + " no encontrado"));
         return toDTO(pedido);
     }
 
-    public PedidoDTO getPedidoByNumero(Integer numero) {
-        Pedido pedido = pedidoRepository.findByNumero(numero)
-                .orElseThrow(() -> new NotFoundException("Pedido con el numero " + numero + " no encontrado"));
+    public PedidoResponseDTO getPedidoByCodigo(String codigoOrden) {
+        Pedido pedido = pedidoRepository.findByCodigoOrden(codigoOrden)
+                .orElseThrow(() -> new NotFoundException("Pedido con el código de orden " + codigoOrden + " no encontrado"));
         return toDTO(pedido);
     }
 
-    public List<PedidoDTO> getAllPedidos() {
+    public List<PedidoResponseDTO> getAllPedidos() {
         List<Pedido> pedidos = pedidoRepository.findAll();
-        List<PedidoDTO> pedidosDTO = new ArrayList<>();
+        List<PedidoResponseDTO> pedidosDTO = new ArrayList<>();
+
         for (Pedido pedido : pedidos) {
-            PedidoDTO pedidoDTO = toDTO(pedido);
+            PedidoResponseDTO pedidoDTO = toDTO(pedido);
             pedidosDTO.add(pedidoDTO);
         }
         return pedidosDTO;
@@ -59,32 +70,6 @@ public class PedidoService {
     public void updatePedido(Long id, PedidoDTO pedidoDTO) {
         Pedido pedido = pedidoRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Pedido con el id " + id + " no encontrado"));
-
-        if (!pedido.getFecha().equals(pedidoDTO.getFecha())) {
-            pedido.setFecha(pedidoDTO.getFecha());
-        }
-
-        if (!pedido.getNumero().equals(pedidoDTO.getNumero())) {
-            pedido.setNumero(pedidoDTO.getNumero());
-        }
-        if (!pedido.getEstado().equals(pedidoDTO.getEstado())) {
-            pedido.setEstado(pedidoDTO.getEstado());
-        }
-        if (!pedido.getHoraEstimadaFin().equals(pedidoDTO.getHoraEstimadaFin())) {
-            pedido.setHoraEstimadaFin(pedidoDTO.getHoraEstimadaFin());
-        }
-        if (!pedido.getTipoEnvio().equals(pedidoDTO.getTipoEnvio())) {
-            pedido.setTipoEnvio(pedidoDTO.getTipoEnvio());
-        }
-        if (!pedido.getTotalVenta().equals(pedidoDTO.getTotalVenta())) {
-            pedido.setTotalVenta(pedidoDTO.getTotalVenta());
-        }
-        if (!pedido.getTotalCosto().equals(pedidoDTO.getTotalCosto())) {
-            pedido.setTotalCosto(pedidoDTO.getTotalCosto());
-        }
-        if (!pedido.getFormaPago().equals(pedidoDTO.getFormaPago())) {
-            pedido.setFormaPago(pedidoDTO.getFormaPago());
-        }
 
         pedidoRepository.save(pedido);
     }
@@ -98,35 +83,81 @@ public class PedidoService {
 
     // MAPPERS
     public Pedido toEntity(PedidoDTO pedidoDTO) {
+
         return Pedido.builder()
                 .fecha(LocalDate.now())
-                .numero(pedidoDTO.getNumero())
-                .estado(pedidoDTO.getEstado())
-                .horaEstimadaFin(null)
+                .hora(LocalTime.now())
+                .codigoOrden(null) // TODO: ACA VER LO DEL TEMA DEL CODIGO
+                .estado(Estado.SOLICITADO) // Estado por defecto al crear un nuevo pedido.
+                .horaEstimadaFin(null) // TODO: ACA VER LO DEL TEMA HORA ESTIMADA FIN
                 .tipoEnvio(pedidoDTO.getTipoEnvio())
-                .totalVenta(pedidoDTO.getTotalVenta())
-                .totalCosto(pedidoDTO.getTotalCosto())
+                .totalVenta(null)
+                .totalCosto(null) // TODO: ACA VER LO DEL TOTAL VENTA
                 .formaPago(pedidoDTO.getFormaPago())
-                .cliente(clienteRepository.findById(pedidoDTO.getCliente().getId())
-                        .orElseThrow(() -> new NotFoundException("Cliente con el id " + pedidoDTO.getCliente().getId() + " no encontrado")))
-                .domicilio(domicilioRepository.findById(pedidoDTO.getDomicilio().getId())
-                        .orElseThrow(() -> new NotFoundException("Domicilio con el id " + pedidoDTO.getDomicilio().getId() + " no encontrado")))
+                .cliente(clienteRepository.findById(pedidoDTO.getClienteId())
+                        .orElseThrow(() -> new NotFoundException("Cliente con el id " + pedidoDTO.getClienteId() + " no encontrado")))
+                .domicilio(domicilioRepository.findById(pedidoDTO.getDomicilioId())
+                        .orElseThrow(() -> new NotFoundException("Domicilio con el id " + pedidoDTO.getDomicilioId() + " no encontrado")))
                 .build();
     }
 
-    public PedidoDTO toDTO(Pedido pedido) {
-        return PedidoDTO.builder()
-                .id(pedido.getId())
+    public PedidoResponseDTO toDTO(Pedido pedido) {
+        return PedidoResponseDTO.builder()
                 .fecha(pedido.getFecha())
-                .numero(pedido.getNumero())
+                .codigoOrden(pedido.getCodigoOrden())
                 .estado(pedido.getEstado())
                 .horaEstimadaFin(pedido.getHoraEstimadaFin())
                 .tipoEnvio(pedido.getTipoEnvio())
                 .totalVenta(pedido.getTotalVenta())
                 .totalCosto(pedido.getTotalCosto())
                 .formaPago(pedido.getFormaPago())
-                .cliente(clienteService.toDto(pedido.getCliente()))
-                .domicilio(domicilioService.toDto(pedido.getDomicilio()))
+                .cliente(clienteService.toDTO(pedido.getCliente()))
+                .domicilio(domicilioService.toDTO(pedido.getDomicilio()))
+                .detallePedidos(pedido.getDetallePedidos().stream()
+                        .map(detallePedidoService::toDTO)
+                        .collect(Collectors.toList()))
                 .build();
     }
+
+    // Metodos adicionales
+    public Double calculateTotalVenta(Pedido pedido) {
+        return pedido.getDetallePedidos().stream()
+                .mapToDouble(DetallePedido::getSubTotal)
+                .sum();
+    }
+
+    public Double calculateTotalCosto(Pedido pedido) {
+        return pedido.getDetallePedidos().stream()
+                .mapToDouble(DetallePedido::getSubTotalCosto)
+                .sum();
+    }
+
+    public LocalTime calculateTiempoEstimadoPreparacion(Pedido pedido) {
+        List<DetallePedido> detallePedidos = pedido.getDetallePedidos();
+        LocalTime mayorTiempo = LocalTime.MIN;
+
+        for (DetallePedido detallePedido : detallePedidos) {
+            Producto producto = detallePedido.getProducto();
+            if (producto != null) {
+                long tiempoEstimado = producto.getTiempoEstimadoPreparacion();
+
+                if (detallePedido.getCantidad() == 2) {
+                    tiempoEstimado = Math.round(tiempoEstimado * 1.25);
+                } else if (detallePedido.getCantidad() >= 3) {
+                    tiempoEstimado = Math.round(tiempoEstimado * 1.5);
+                }
+
+                // Convertimos minutos a segundos, luego a LocalTime
+                LocalTime tiempoPreparacion = LocalTime.ofSecondOfDay(tiempoEstimado * 60);
+
+                if (tiempoPreparacion.isAfter(mayorTiempo)) {
+                    mayorTiempo = tiempoPreparacion;
+                }
+            }
+        }
+
+        // tiempo de prep = el mayor + 10 minutos
+        return mayorTiempo.plusMinutes(10);
+    }
+
 }
