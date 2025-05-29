d6b2c3b1778c132291ff201a0baeb766f243ea92
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-05-29 19:02:26 -0300

    factura controller generico

--- src/main/java/org/spdgrupo/elbuensaborapi/controller/FacturaController.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/controller/FacturaController.java
@@ -1,14 +1,28 @@
 package org.spdgrupo.elbuensaborapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.spdgrupo.elbuensaborapi.model.dto.factura.FacturaDTO;
+import org.spdgrupo.elbuensaborapi.model.dto.factura.FacturaResponseDTO;
+import org.spdgrupo.elbuensaborapi.model.entity.Factura;
 import org.spdgrupo.elbuensaborapi.service.FacturaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/facturas")
-@RequiredArgsConstructor
-public class FacturaController {
+public class FacturaController extends GenericoControllerImpl<
+        Factura,
+        FacturaDTO,
+        FacturaResponseDTO,
+        Long,
+        FacturaService> {
 
-    private final FacturaService facturaService;
+    @Autowired
+    private FacturaService facturaService;
+
+    public FacturaController(FacturaService facturaService) {
+        super(facturaService);
+        this.facturaService = facturaService;
+    }
 
 }
