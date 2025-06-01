92d83d8d95f6a93b9122fa36a886dd6ef31c6eb3
Alvaro Rubina <alvarorubina132@gmail.com>
2025-06-01 16:33:34 -0300

    Optimizada la forma en que se guardan Domicilios para Clientes

--- src/main/java/org/spdgrupo/elbuensaborapi/config/mappers/DetalleDomicilioMapper.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/config/mappers/DetalleDomicilioMapper.java
@@ -1,6 +1,7 @@
 package org.spdgrupo.elbuensaborapi.config.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.spdgrupo.elbuensaborapi.model.dto.detalledomicilio.DetalleDomicilioDTO;
 import org.spdgrupo.elbuensaborapi.model.dto.detalledomicilio.DetalleDomicilioResponseDTO;
 import org.spdgrupo.elbuensaborapi.model.entity.DetalleDomicilio;
@@ -8,6 +9,11 @@ import org.spdgrupo.elbuensaborapi.model.interfaces.GenericoMapper;
 
 @Mapper(componentModel = "spring", uses = {DomicilioMapper.class})
 public interface DetalleDomicilioMapper extends GenericoMapper<DetalleDomicilio, DetalleDomicilioDTO, DetalleDomicilioResponseDTO> {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "clienteId", target = "cliente.id")
+    @Mapping(source = "domicilio", target = "domicilio")
+    DetalleDomicilio toEntity(DetalleDomicilioDTO detalleDomicilioDTO);
 
     DetalleDomicilioResponseDTO toResponseDTO(DetalleDomicilio detalleDomicilio);
 }
--- src/main/java/org/spdgrupo/elbuensaborapi/controller/DetalleDomicilioController.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/controller/DetalleDomicilioController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/detalleDomicilio")
+@RequestMapping("/api/detalledomicilios")
 public class DetalleDomicilioController extends GenericoControllerImpl<
         DetalleDomicilio,
         DetalleDomicilioDTO,
