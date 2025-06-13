5bc1e518fc573faaf02d6d502f8dc0cd7b4b70ae
Alvaro Rubina <alvarorubina132@gmail.com>
2025-06-12 22:26:58 -0300

    Actualizacion dotenv

--- src/main/java/org/spdgrupo/elbuensaborapi/model/enums/Estado.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/model/enums/Estado.java
@@ -1,8 +1,7 @@
 package org.spdgrupo.elbuensaborapi.model.enums;
 
 public enum Estado {
-    PENDIENTE_FACTURACION,
-    SOLICITADO, // Este es el estado cuando ya ha sido pagado sea efectivo/mp
+    SOLICITADO,
     EN_PREPARACION,
     TERMINADO,
     EN_CAMINO,
