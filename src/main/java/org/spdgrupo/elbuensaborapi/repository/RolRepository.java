9e4cc1853c558e2cc518fe420022781bb224a300
Alvaro Rubina <alvarorubina132@gmail.com>
2025-06-14 15:27:14 -0300

    Creados RolMapper y RolRepository

---
+++ src/main/java/org/spdgrupo/elbuensaborapi/repository/RolRepository.java
@@ -0,0 +1,14 @@
+package org.spdgrupo.elbuensaborapi.repository;
+
+import org.spdgrupo.elbuensaborapi.model.entity.Rol;
+import org.spdgrupo.elbuensaborapi.model.interfaces.GenericoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RolRepository extends GenericoRepository<Rol, Long> {
+    Optional<Rol> findByAuth0RolId(String name);
+    Optional<Rol> findByNombre(String name);
+
+}
