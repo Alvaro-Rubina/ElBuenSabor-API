e0371fc17401b09c5d8a237da861bd919a00298a
Alvaro Rubina <alvarorubina132@gmail.com>
2025-06-15 13:19:36 -0300

    Añadidos metodos faltantes a ClienteController y EmpleadoController

--- src/main/java/org/spdgrupo/elbuensaborapi/controller/ClienteController.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/controller/ClienteController.java
@@ -1,42 +1,69 @@
 package org.spdgrupo.elbuensaborapi.controller;
 
+import com.auth0.exception.Auth0Exception;
 import jakarta.validation.Valid;
 import org.spdgrupo.elbuensaborapi.model.dto.cliente.ClienteDTO;
 import org.spdgrupo.elbuensaborapi.model.dto.cliente.ClienteResponseDTO;
-import org.spdgrupo.elbuensaborapi.model.entity.Cliente;
 import org.spdgrupo.elbuensaborapi.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/clientes")
-public class ClienteController extends GenericoControllerImpl<
-        Cliente,
-        ClienteDTO,
-        ClienteResponseDTO,
-        Long,
-        ClienteService> {
+public class ClienteController {
 
     @Autowired
     private ClienteService clienteService;
 
-    public ClienteController(ClienteService clienteService) {
-        super(clienteService);
-        this.clienteService = clienteService;
+    @PostMapping("/save")
+    public ResponseEntity<String> save(@Valid @RequestBody ClienteDTO clienteDTO) throws Auth0Exception {
+        clienteService.save(clienteDTO);
+        return ResponseEntity.ok("Registro exitoso");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteResponseDTO> findById(@PathVariable Long id) {
+        ClienteResponseDTO cliente = clienteService.findById(id);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<ClienteResponseDTO> findByEmail(@PathVariable String email) {
+        ClienteResponseDTO cliente = clienteService.findByEmail(email);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteResponseDTO>> findAll() {
+        return ResponseEntity.ok(clienteService.findAll());
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<String> updateCliente(@PathVariable Long id,
-                                                @Valid @RequestBody ClienteDTO clienteDTO) {
+    public ResponseEntity<String> update(@PathVariable Long id,
+                                                @Valid @RequestBody ClienteDTO clienteDTO) throws Auth0Exception {
         clienteService.update(id, clienteDTO);
         return ResponseEntity.ok("Cliente actualizado correctamente");
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteCliente(@PathVariable Long id) {
+    public ResponseEntity<String> delete(@PathVariable Long id) {
         clienteService.delete(id);
         return ResponseEntity.ok("Cliente eliminado exitosamente");
+    }
+
+    @DeleteMapping("delete/physical/{id}")
+    public ResponseEntity<String> deletePhysically(@PathVariable Long id) throws Auth0Exception {
+        clienteService.deletePhysically(id);
+        return ResponseEntity.ok("Cliente eliminado fisicamente");
+    }
+
+    @PutMapping("/toggle-activo/{id}")
+    public ResponseEntity<String> toggleActivo(@PathVariable Long id) {
+        clienteService.toggleActivo(id);
+        return ResponseEntity.ok("Cliente actualizado exitosamente");
     }
 
 }
