67e33d8917415c107c885fe6816391f28cfce07b
Alvaro Rubina <alvarorubina132@gmail.com>
2025-06-15 13:22:07 -0300

    Añadido metodo findByDenominacion en ProductoService

--- src/main/java/org/spdgrupo/elbuensaborapi/repository/ProductoRepository.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/repository/ProductoRepository.java
@@ -5,9 +5,11 @@ import org.spdgrupo.elbuensaborapi.model.interfaces.GenericoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductoRepository extends GenericoRepository<Producto, Long> {
+    Optional<Producto> findByDenominacion(String denominacion);
     List<Producto> findByDenominacionContainingIgnoreCase(String denominacion);
 
     List<Producto> findByRubroId(Long rubroId);
