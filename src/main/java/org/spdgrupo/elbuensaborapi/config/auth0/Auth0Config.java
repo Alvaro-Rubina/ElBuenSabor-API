af543e956b9a13cb6f881afd28ff9bdefb24bf5e
Alvaro Rubina <alvarorubina132@gmail.com>
2025-06-15 01:46:16 -0300

    Actualizado UsuarioMapper

--- src/main/java/org/spdgrupo/elbuensaborapi/config/auth0/Auth0Config.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/config/auth0/Auth0Config.java
@@ -2,6 +2,7 @@ package org.spdgrupo.elbuensaborapi.config.auth0;
 
 import com.auth0.client.auth.AuthAPI;
 import com.auth0.client.mgmt.ManagementAPI;
+import com.auth0.exception.Auth0Exception;
 import com.auth0.json.auth.TokenHolder;
 import com.auth0.net.AuthRequest;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,7 +24,7 @@ public class Auth0Config {
     private ManagementAPI managementAPI;
 
     @Bean
-    public ManagementAPI managementAPI() throws Exception {
+    public ManagementAPI managementAPI() throws Auth0Exception {
         AuthAPI authAPI = new AuthAPI(domain, clientId, clientSecret);
         AuthRequest request = authAPI.requestToken("https://" + domain + "/api/v2/");
         TokenHolder holder = request.execute();
--- src/main/java/org/spdgrupo/elbuensaborapi/config/mappers/UsuarioMapper.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/config/mappers/UsuarioMapper.java
@@ -1,16 +1,29 @@
 package org.spdgrupo.elbuensaborapi.config.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.spdgrupo.elbuensaborapi.model.dto.usuario.UsuarioDTO;
 import org.spdgrupo.elbuensaborapi.model.dto.usuario.UsuarioResponseDTO;
+import org.spdgrupo.elbuensaborapi.model.entity.Rol;
 import org.spdgrupo.elbuensaborapi.model.entity.Usuario;
 import org.spdgrupo.elbuensaborapi.model.interfaces.GenericoMapper;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper extends GenericoMapper<Usuario, UsuarioDTO, UsuarioResponseDTO> {
 
-    Usuario toEntity(UsuarioDTO usuarioDTO);
-
+    @Mapping(target = "roles", source = "roles")
     UsuarioResponseDTO toResponseDTO(Usuario usuario);
 
+    @Mapping(target = "roles", ignore = true) // El mapeo inverso lo manejas en el service
+    Usuario toEntity(UsuarioDTO usuarioDTO);
+
+    default List<String> map(Set<Rol> roles) {
+        return roles == null ? null : roles.stream()
+            .map(Rol::getAuth0RolId)
+            .collect(Collectors.toList());
+    }
 }
