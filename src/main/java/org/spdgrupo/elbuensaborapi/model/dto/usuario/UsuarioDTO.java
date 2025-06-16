dfbca5efd7b989aaa0d24c24bde52f7eba87870e
Alvaro Rubina <alvarorubina132@gmail.com>
2025-06-16 10:59:03 -0300

    Añadido campo nickName a UsuarioDTO

--- src/main/java/org/spdgrupo/elbuensaborapi/model/dto/usuario/UsuarioDTO.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/model/dto/usuario/UsuarioDTO.java
@@ -1,5 +1,6 @@
 package org.spdgrupo.elbuensaborapi.model.dto.usuario;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.util.List;
@@ -12,17 +13,16 @@ import java.util.List;
 public class UsuarioDTO {
 
     // TODO: Esto tendría que ser opcional? por lo de registrarse con google
-    /*@NotBlank(message = "El campo email no puede estar vacio")*/
+    @NotBlank(message = "El campo email no puede estar vacio")
     private String email;
 
     private String nombreCompleto;
 
-    // TODO: Esto tendría que ser opcional? por lo de registrarse con google
-    /*@NotBlank(message = "El campo contraseña no puede estar vacio")*/
     private String contrasenia;
 
-    // Esto no se si tiene que ir
     private String connection;
+
+    private String nickName;
 
     private String auth0Id;
 
