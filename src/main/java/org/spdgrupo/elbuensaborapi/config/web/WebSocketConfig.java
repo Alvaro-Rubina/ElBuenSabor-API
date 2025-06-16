d853a9b72a2e01e1350deaec900becb0dfa0c735
Alvaro Rubina <alvarorubina132@gmail.com>
2025-06-16 12:42:42 -0300

    Eliminada configuracion para SockJS

--- src/main/java/org/spdgrupo/elbuensaborapi/config/web/WebSocketConfig.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/config/web/WebSocketConfig.java
@@ -11,8 +11,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://localhost:5173")
-                .withSockJS();
+                .setAllowedOrigins("https://localhost:5173");
     }
 
     @Override
