303faa675f1fb0b433c288c9711414a82d3046cd
Alvaro Rubina <alvarorubina132@gmail.com>
2025-06-21 16:46:05 -0300

    Agregado metodo para buscar una factura segun el id del pedido

--- src/main/java/org/spdgrupo/elbuensaborapi/repository/FacturaRepository.java
+++ src/main/java/org/spdgrupo/elbuensaborapi/repository/FacturaRepository.java
@@ -4,7 +4,10 @@ import org.spdgrupo.elbuensaborapi.model.entity.Factura;
 import org.spdgrupo.elbuensaborapi.model.interfaces.GenericoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FacturaRepository extends GenericoRepository<Factura, Long> {
+    Optional<Factura> findByPedido_Id(Long id);
 
 }
